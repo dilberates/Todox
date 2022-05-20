37177ca55fb648e04927abe1cdf19c97483f2ae7
zulalataman <zulalatamaaan@gmail.com>
2022-05-21 02:13:00 +0300

    Notlar
    Notlar bölümü tamamlandı

---
+++ app/src/main/java/com/example/myapplication/createNote.java
@@ -0,0 +1,100 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class createNote extends AppCompatActivity {
+
+    EditText mcreatetitleofnote,mcreatecontentofnote;
+    FloatingActionButton msavenote;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        msavenote=findViewById(R.id.savenote);
+        mcreatecontentofnote=findViewById(R.id.createcontentofnote);
+        mcreatetitleofnote=findViewById(R.id.createtitleofnote);
+
+        Toolbar toolbar=findViewById(R.id.toolbarofcreatenote);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+
+        msavenote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title=mcreatetitleofnote.getText().toString();
+                String content=mcreatecontentofnote.getText().toString();
+                if(title.isEmpty() || content.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"Lütfen her iki alanı da doldurunuz!",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    DocumentReference documentReference=firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").document();
+                    Map<String ,Object> note= new HashMap<>();
+                    note.put("title",title);
+                    note.put("content",content);
+
+                    documentReference.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getApplicationContext(),"Not kaydedildi",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(createNote.this,notesActivitiy.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),"Not kaydedilemedi",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+
+
+                }
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home)
+        {
+            startActivity(new Intent(this, notesActivitiy.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
