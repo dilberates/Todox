fe0a44c6e0e7f1c21e8250fd6e41aa1af9b9c800
Dilber KILIÇ <dilberklc5252@gmail.com>
2022-05-21 14:09:35 +0300

    update
    fireabase bağlantısı yapıldı.

--- app/src/main/java/com/example/myapplication/Model_todo.java
+++ app/src/main/java/com/example/myapplication/Model_todo.java
@@ -1,4 +1,48 @@
 package com.example.myapplication;
 
+
 public class Model_todo {
+    private String gorev,aciklama,id,date;
+    public Model_todo(){
+
+    }
+
+    public Model_todo(String gorev, String aciklama, String id, String date) {
+        this.gorev = gorev;
+        this.aciklama = aciklama;
+        this.id = id;
+        this.date = date;
+    }
+
+    public String getGorev() {
+        return gorev;
+    }
+
+    public void setGorev(String gorev) {
+        this.gorev = gorev;
+    }
+
+    public String getAciklama() {
+        return aciklama;
+    }
+
+    public void setAciklama(String aciklama) {
+        this.aciklama = aciklama;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- app/src/main/java/com/example/myapplication/ToDoActivity.java
+++ app/src/main/java/com/example/myapplication/ToDoActivity.java
@@ -1,22 +1,51 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.xml.transform.sax.SAXResult;
 
 public class ToDoActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private RecyclerView recyclerView;
     private FloatingActionButton floatingActionButton;
+
+    private DatabaseReference reference;
+    private FirebaseAuth myAuth;
+    private FirebaseUser mUser;
+    private String onlineUserID;
+
+    private ProgressDialog loader;
+
+    private String key="";
+    private String gorev;
+    private  String aciklama;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +58,8 @@ public class ToDoActivity extends AppCompatActivity {
         linearLayoutManager.setStackFromEnd(true);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
+        loader=new ProgressDialog(this);
+
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,14 +67,67 @@ public class ToDoActivity extends AppCompatActivity {
             }
 
         });
+
     }
     private void addTask() {
         AlertDialog.Builder myDialog=new AlertDialog.Builder(this);
         LayoutInflater inflater=LayoutInflater.from(this);
+
         View myView=inflater.inflate(R.layout.input_file_todo,null);
         myDialog.setView(myView);
-        AlertDialog dialog=myDialog.create();
+
+        final AlertDialog dialog=myDialog.create();
         dialog.setCancelable(false);
+        final EditText gorevtxt=myView.findViewById(R.id.gorev);
+        final EditText aciklamatxt=myView.findViewById(R.id.aciklama);
+        Button savebtn=myView.findViewById(R.id.ekleBtn);
+        Button cancelbtn=myView.findViewById(R.id.iptalBtn);
+
+        cancelbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mgorev=gorevtxt.getText().toString().trim();
+                String maciklama=aciklamatxt.getText().toString().trim();
+                String id=reference.push().getKey();
+                String date= DateFormat.getDateInstance().format(new Date());
+
+                if(TextUtils.isEmpty(mgorev)){
+                    gorevtxt.setError("Lütfen Görev Giriniz !!");
+                    return;
+                }
+                if(TextUtils.isEmpty(maciklama)){
+                    gorevtxt.setError("Lütfen Açıklama Giriniz !!");
+                    return;
+                }else{
+                 loader.setMessage("Verileriniz ekleniyor..");
+                 loader.setCanceledOnTouchOutside(false);
+                 loader.show();
+
+                 Model_todo model=new Model_todo(mgorev,maciklama,id,date);
+                 reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                     @Override
+                     public void onComplete(@NonNull Task<Void> task) {
+                         if(task.isSuccessful())
+                         {
+                             Toast.makeText(ToDoActivity.this,"Görev başarı ile kaydedildi.",Toast.LENGTH_SHORT).show();
+                             loader.dismiss();
+                         }else{
+                             String error=task.getException().toString();
+                             Toast.makeText(ToDoActivity.this,"Hata",Toast.LENGTH_SHORT).show();
+                             loader.dismiss();
+                         }
+                     }
+                 });
+                }
+            }
+        });
         dialog.show();
 
     }
@@ -52,6 +136,10 @@ public class ToDoActivity extends AppCompatActivity {
         toolbar=findViewById(R.id.homeToolBar);
         recyclerView=findViewById(R.id.recyclerView);
         floatingActionButton=findViewById(R.id.flo);
+        myAuth=FirebaseAuth.getInstance();
+        mUser=myAuth.getCurrentUser();
+        onlineUserID=mUser.getUid();
+        reference= FirebaseDatabase.getInstance().getReference().child("gorevler").child(onlineUserID);
 
     }
 }
